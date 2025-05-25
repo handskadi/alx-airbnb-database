7b41ab7f4019fd9a620e47e42f0a10597c50540c
Mohamed KADI <handskadi@gmail.com>
2025-05-25 16:00:12 +0100

    Fix task 2: apply RANK() correctly using CTE and window functioN

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,9 +3,12 @@ SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
 
--- Rank properties by bookings using RANK()
-SELECT property_id,
-       COUNT(*) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
-FROM bookings
-GROUP BY property_id;
+-- Rank properties by total number of bookings using RANK()
+WITH booking_counts AS (
+  SELECT property_id, COUNT(*) AS total_bookings
+  FROM bookings
+  GROUP BY property_id
+)
+SELECT property_id, total_bookings,
+       RANK() OVER (ORDER BY total_bookings DESC) AS rank
+FROM booking_counts;
--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -15,22 +15,3 @@ SELECT u.*, b.*
 FROM users u
 FULL OUTER JOIN bookings b ON u.id = b.user_id
 ORDER BY u.id;
-
--- Properties with avg rating > 4.0
-SELECT id, title
-FROM properties
-WHERE id IN (
-  SELECT property_id
-  FROM reviews
-  GROUP BY property_id
-  HAVING AVG(rating) > 4.0
-);
-
--- Users with more than 3 bookings (correlated)
-SELECT *
-FROM users u
-WHERE (
-  SELECT COUNT(*)
-  FROM bookings b
-  WHERE b.user_id = u.id
-) > 3;
